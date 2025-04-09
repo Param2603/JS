72e946614f40d19965872cd33381e23bb64634dc
Parth99001 <parthgandhi350@gmail.com>
2025-04-09 16:17:39 +0530

    que

---
+++ Question/factory function/index.js
@@ -0,0 +1,47 @@
+// Q-1: E-commerce Inventory Management:
+// Question: Imagine you have an e-commerce site that sells electronics. You need a function that creates a product with properties like name, price, and category. Create a factory function called createProduct that returns an object for each product. Hint: Think of the factory function as a "blueprint" that creates different product objects with specified properties.
+
+function createProduct (name,Price,category) {
+    let product = {}
+    product.name = name;
+    product.price = Price;
+    product.category = category;
+
+    return product;
+}
+console.log("Q-1 Output")
+
+let mobile = createProduct("Nothing",20000,"android")
+console.log(mobile)
+
+let computer = createProduct("lenovo",40000,"2016")
+console.log(computer)
+
+// Q-2: Student Report Card Generator:
+// Question: Write a factory function createReportCard that generates a report card for each student with properties such as name, grade, and subject. This function should return an object for each student. Hint: Each student can have different grades, so think about how each object represents an individual student's information.
+
+function createReportCard  (name,grade,subject){
+    let object = {}
+    object.name = name;
+    object.grade = grade;
+    object.subject = subject;
+
+    return object;
+}
+console.log("Q-2 Output")
+
+let paramData = createReportCard ("Param",90,"math")
+console.log(paramData)
+
+let dhruvData =  createReportCard ("Dhruv",85,"accounts")
+console.log(dhruvData)
+
+// Car Rental Service:
+// Question: Suppose you run a car rental service. Write a factory function createCar that creates a car object with properties like model, rentalRate, and availability. Use this to generate car instances for each vehicle in your fleet. Hint: Each car will have different values for these properties, so use a function that takes parameters for each property.
+
+function createCar (model,rentalRate,availability){
+    let car = {}
+    car.model = model;
+    car.rentalRate =rentalRate;
+    car.availability = availability;
+}
