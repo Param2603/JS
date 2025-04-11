5f67830ccfa1e6931b44474c8b4b77647823f432
Parth99001 <parthgandhi350@gmail.com>
2025-04-11 16:00:17 +0530

    que

---
+++ Question/constructor function/index.js
@@ -0,0 +1,16 @@
+// Q-1: Hotel Room Reservation System:
+// Question: Suppose you’re building a reservation system for a hotel. Write a constructor function Room that creates a room object with properties like roomNumber, type, price, and isAvailable. Hint: Use the new keyword to create new room objects.
+
+
+
+// Q-2: Online Course Management:
+// Question: Design a constructor function Course that generates an object representing an online course with properties like title, instructor, duration, and studentsEnrolled. Hint: Use the this keyword to assign values to each property.
+
+// Q-3: Digital Wallet:
+// Question: Create a constructor function Wallet that initializes a wallet object with properties like ownerName, balance, and currency. Include a method that allows the owner to add money to the wallet. Hint: Add methods inside the constructor to handle wallet operations.
+
+// Q-4: City Weather Tracker:
+// Question: Write a constructor function Weather that creates a weather object with properties like city, temperature, humidity, and forecast. Create a method to update the forecast. Hint: Use the constructor to set initial values, and add a method to update properties.
+
+// Q-5: School Student Enrolment:
+// Question: Write a constructor function Student that generates a student object with properties name, rollNumber, grade, and subjects. Add a method to add new subjects to the student’s subject list. Hint: Use the new keyword to create each student, and add a method to modify the object’s state.
