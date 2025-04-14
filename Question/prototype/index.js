0370d5cf722465e6b6afa8ada0622b4d9667c360
Parth99001 <parthgandhi350@gmail.com>
2025-04-14 16:00:12 +0530

    que

---
+++ Question/prototype/index.js
@@ -0,0 +1,14 @@
+// Q-1. Vehicle Constructor
+// Create a Vehicle constructor function in JavaScript that takes type, wheels, and speed as parameters. Use the prototype to add a method called describe that prints out, "This is a [type] with [wheels] wheels, and it can go up to [speed] km/h."
+
+// Q-2.  Library Book Constructor
+// Design a Book constructor function that accepts title, author, and year. Add a prototype method called getBookInfo that returns a string like, "Title: [title], Author: [author], Year: [year]."
+  
+// Q-3. Bank Account Constructor
+// Build a BankAccount constructor function with accountNumber, balance, and accountHolder as properties. Add a prototype method deposit(amount) that increases the balance by the given amount and logs the new balance.
+
+// Q-4. Employee Constructor
+// Create an Employee constructor function with name, position, and salary properties. Use prototypes to add a giveRaise method that increases the employee's salary by a given percentage and logs the updated salary.
+
+// Q-5. Movie Constructor
+// Construct a Movie function that takes in title, genre, and rating as parameters. Use the prototype to add a method rate(newRating) that updates the movie's rating and logs, "The new rating for [title] is [newRating]."
