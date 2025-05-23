1b548fdc3a2decd5aaccfce6efb9b6c2ed0e29dd
Param2603 <ps26032005@gmail.com>
2025-05-23 07:24:49 -0700

    addtocart

--- project/AddToCart/style.js
+++ project/AddToCart/style.js
@@ -1,6 +1,9 @@
 let iconCart = document.querySelector('.icon-cart');
 let closeCart =  document.querySelector('.close');
 let body = document.querySelector('body');
+let listProduct = document.querySelector('.listProduct');
+
+let listProducts = []
 
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
@@ -9,3 +12,30 @@ iconCart.addEventListener('click', () => {
 closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
+
+const addDataToHTML = () => {
+    listProducts.innerHTML = "";
+    if(listProduct.length > 0){
+        listProduct.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item')
+            newProduct.innerHTML = `
+               <img src="$(product.image)" alt="">
+                <h2>$(product.name)</h2>
+                <div class="price">$(product.price)</div>
+                <button class="addcart">Add To Cart</button>`;
+                listProduct.appendChild(newProduct);
+        })
+    }
+
+}
+
+const initApp = () => {
+    fetch('product.json')
+    .then(response => response.json())
+    .then(data => {
+        listProduct = data;
+        addDataToHTML();
+    })   
+}
+initApp();
