61e7e53474b0b72fc3752cd343898c22d498f53d
Param2603 <ps26032005@gmail.com>
2025-05-25 20:46:58 -0700

    addtocart

--- project/AddToCart/style.js
+++ project/AddToCart/style.js
@@ -27,7 +27,7 @@ const addDataToHTML = () => {
             newProduct.innerHTML = `
             <img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
-                <div class="price">$${product.price}</div>
+                <div class="price">₹${product.price}</div>
                 <button class="addcart">Add To Cart</button>`;    
                 listProductHTML.appendChild(newProduct)
         })
